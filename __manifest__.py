44eac469964d130047dffaa66982142feff21226
aaronNgu <aaronngu@hotmail.com>
2026-02-12 09:53:35 -0800

    [IMP] website_sale_partner_firstname: apply OCA guidelines

--- __manifest__.py
+++ __manifest__.py
@@ -2,14 +2,14 @@
 {
     'name': "First & Last Name at Checkout",
     'summary': "Separate first and last name fields at checkout and portal",
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Aaron Ngu",
     'category': 'Website/Website',
     'version': '18.0.1.0.0',
     'license': 'LGPL-3',
+    'images': [],
     'depends': ['website_sale', 'partner_firstname'],
     'data': [
-        'data/whitelist_fields.xml',
+        'data/res_partner_data.xml',
         'views/templates.xml',
     ],
 }
